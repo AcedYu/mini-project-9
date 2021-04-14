d9d87d6e9ff1a71ed0c037b03c14e8e598bfed29
AcedYu <alexyu299@hotmail.com>
2021-04-14 11:57:01 -0700

    replaced CSS with bootstrap

--- index.js
+++ index.js
@@ -25,30 +25,24 @@ const writeUp = (data) => {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
       <title>Document</title>
-          <style>
-              .box {
-                  border: 1px solid #000;
-                  padding: 20px;
-                  width: 500px;
-                  margin: 0 auto;
-                  margin-top: 20px;
-                  text-align: center;
-                  border-radius: 10px;
-                  background-color: lightgrey
-              }
-          </style>
   </head>
 
   <body>
-      <div class="box">
-        <h1>${data.name}</h1>
-        <p>${data.location}</p>
-        <h2>${data.gitHub}</h2>
-        <h2>${data.linkedIn}</h2>
-        <h2>${data.email}</h2>
-        <p>${data.description}</p>
+    <div class="card" style="width: 18rem;">
+      <div class="card-header">
+        ${data.name}
       </div>
+      <div class="card-body">
+        <h5 class="card-title">${data.name}</h5>
+        <p class="card-text">${data.location}</p>
+        <p class="card-text">${data.gitHub}</p>
+        <p class="card-text">${data.linkedIn}</p>
+        <p class="card-text">${data.email}</p>
+        <p class="card-text">${data.description}</p>
+      </div>
+    </div>
   </body>
   </html>
   `, err => console.log(err));
